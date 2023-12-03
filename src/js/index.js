24444b4686ff319f0f50b79fa024a5bc8357c517
fcenciel14 <fcenciel14@gmail.com>
2023-12-03 17:11:16 +0900

    feat: TicTacToeクラスを作成

--- src/js/index.js
+++ src/js/index.js
@@ -3,30 +3,47 @@ function handleClick(row, col) {
   const cell = document.getElementById(`cell-${row}-${col}`);
 
   // すでに埋まっているマスをクリックした場合何もしない
-  if (!board.isEmpty(row, col)) {
+  if (!ticTacToe.board.isEmpty(row, col)) {
     return;
   }
-  board.placeSymbol(row, col, cell);
-  board.checkWinner();
-  board.switchPlayer();
+  ticTacToe.board.placeSymbol(row, col, cell);
+  ticTacToe.board.checkWinner();
+  ticTacToe.switchPlayer();
 }
 
 // TODO: クラスファイルに切り出す
-class Board {
+class TicTacToe {
   constructor() {
-    // nullで初期化された3x3の配列を生成
-    this.cells = Array(3)
-      .fill(null)
-      .map(() => Array(3).fill(null));
+    this.board = null;
+  }
+
+  start() {
+    this.board = new Board(this);
 
     // プレイヤーを作成
     this.players = {
       x: new HumanPlayer('x'),
       o: new HumanPlayer('o'),
     };
-
     // 現在の手番を保持
     this.currentPlayer = this.players.x;
+  }
+
+  // 手番の更新
+  switchPlayer() {
+    this.currentPlayer = this.currentPlayer === this.players.x ? this.players.o : this.players.x;
+  }
+}
+
+// TODO: クラスファイルに切り出す
+class Board {
+  constructor(game) {
+    // nullで初期化された3x3の配列を生成
+    this.cells = Array(3)
+      .fill(null)
+      .map(() => Array(3).fill(null));
+
+    this.game = game;
   }
 
   // セルが空白か確認
@@ -37,14 +54,9 @@ class Board {
   // 一手進める
   placeSymbol(row, col, cell) {
     // プログラム側で保持している配列の更新
-    this.cells[row][col] = this.currentPlayer.symbol;
+    this.cells[row][col] = this.game.currentPlayer.symbol;
     // UIの更新
-    cell.textContent = this.currentPlayer.symbol;
-  }
-
-  // 手番の更新
-  switchPlayer() {
-    this.currentPlayer = this.currentPlayer === this.players.x ? this.players.o : this.players.x;
+    cell.textContent = this.game.currentPlayer.symbol;
   }
 
   // 勝敗の確認
@@ -52,39 +64,39 @@ class Board {
     for (let i = 0; i < 3; i++) {
       // 横が揃う
       if (
-        this.cells[i][0] === this.currentPlayer.symbol &&
-        this.cells[i][1] === this.currentPlayer.symbol &&
-        this.cells[i][2] === this.currentPlayer.symbol
+        this.cells[i][0] === this.game.currentPlayer.symbol &&
+        this.cells[i][1] === this.game.currentPlayer.symbol &&
+        this.cells[i][2] === this.game.currentPlayer.symbol
       ) {
-        console.log(`Winner: ${this.currentPlayer.symbol}`);
+        console.log(`Winner: ${this.game.currentPlayer.symbol}`);
       }
 
       // 縦が揃う
       if (
-        this.cells[0][i] === this.currentPlayer.symbol &&
-        this.cells[1][i] === this.currentPlayer.symbol &&
-        this.cells[2][i] === this.currentPlayer.symbol
+        this.cells[0][i] === this.game.currentPlayer.symbol &&
+        this.cells[1][i] === this.game.currentPlayer.symbol &&
+        this.cells[2][i] === this.game.currentPlayer.symbol
       ) {
-        console.log(`Winner: ${this.currentPlayer.symbol}`);
+        console.log(`Winner: ${this.game.currentPlayer.symbol}`);
       }
     }
 
     // 斜めが揃う
     if (
-      this.cells[0][0] === this.currentPlayer.symbol &&
-      this.cells[1][1] === this.currentPlayer.symbol &&
-      this.cells[2][2] === this.currentPlayer.symbol
+      this.cells[0][0] === this.game.currentPlayer.symbol &&
+      this.cells[1][1] === this.game.currentPlayer.symbol &&
+      this.cells[2][2] === this.game.currentPlayer.symbol
     ) {
-      console.log(`Winner: ${this.currentPlayer.symbol}`);
+      console.log(`Winner: ${this.game.currentPlayer.symbol}`);
     }
 
     // 斜めが揃う
     if (
-      this.cells[0][2] === this.currentPlayer.symbol &&
-      this.cells[1][1] === this.currentPlayer.symbol &&
-      this.cells[2][0] === this.currentPlayer.symbol
+      this.cells[0][2] === this.game.currentPlayer.symbol &&
+      this.cells[1][1] === this.game.currentPlayer.symbol &&
+      this.cells[2][0] === this.game.currentPlayer.symbol
     ) {
-      console.log(`Winner: ${this.currentPlayer.symbol}`);
+      console.log(`Winner: ${this.game.currentPlayer.symbol}`);
     }
   }
 }
@@ -100,7 +112,6 @@ class Player {
 class HumanPlayer extends Player {}
 class ComputerPlayer extends Player {}
 
-const board = new Board();
-// const playerX = new HumanPlayer('x');
-// const playerO = new HumanPlayer('o');
-// let currentPlayer = playerX;
+// TODO: スタートボタンを後々実装する
+const ticTacToe = new TicTacToe();
+ticTacToe.start();
