6c350e290c7751ef39732b694e066ff8fcc7b709
fcenciel14 <fcenciel14@gmail.com>
2023-12-03 16:14:19 +0900

    feat: セルの空白確認の実装、グローバル変数をクラスプロパティに変更

--- src/js/index.js
+++ src/js/index.js
@@ -1,71 +1,90 @@
 function handleClick(row, col) {
   // セルの取得
   const cell = document.getElementById(`cell-${row}-${col}`);
-  // 一手進める
+
+  // すでに埋まっているマスをクリックした場合何もしない
+  if (!board.isEmpty(row, col)) {
+    return;
+  }
   board.placeSymbol(row, col, cell);
-  // 勝利判定
   board.checkWinner();
-  // 手番の更新
   board.switchPlayer();
 }
 
 // TODO: クラスファイルに切り出す
 class Board {
-  // nullで初期化された3x3の配列を生成
   constructor() {
+    // nullで初期化された3x3の配列を生成
     this.cells = Array(3)
       .fill(null)
       .map(() => Array(3).fill(null));
+
+    // プレイヤーを作成
+    this.players = {
+      x: new HumanPlayer('x'),
+      o: new HumanPlayer('o'),
+    };
+
+    // 現在の手番を保持
+    this.currentPlayer = this.players.x;
   }
 
+  // セルが空白か確認
+  isEmpty(row, col) {
+    return this.cells[row][col] === null;
+  }
+
+  // 一手進める
   placeSymbol(row, col, cell) {
     // プログラム側で保持している配列の更新
-    this.cells[row][col] = currentPlayer.symbol;
+    this.cells[row][col] = this.currentPlayer.symbol;
     // UIの更新
-    cell.textContent = currentPlayer.symbol;
+    cell.textContent = this.currentPlayer.symbol;
   }
 
+  // 手番の更新
   switchPlayer() {
-    currentPlayer = currentPlayer === playerX ? playerO : playerX;
+    this.currentPlayer = this.currentPlayer === this.players.x ? this.players.o : this.players.x;
   }
 
+  // 勝敗の確認
   checkWinner() {
     for (let i = 0; i < 3; i++) {
       // 横が揃う
       if (
-        this.cells[i][0] === currentPlayer.symbol &&
-        this.cells[i][1] === currentPlayer.symbol &&
-        this.cells[i][2] === currentPlayer.symbol
+        this.cells[i][0] === this.currentPlayer.symbol &&
+        this.cells[i][1] === this.currentPlayer.symbol &&
+        this.cells[i][2] === this.currentPlayer.symbol
       ) {
-        console.log(`Winner: ${currentPlayer.symbol}`);
+        console.log(`Winner: ${this.currentPlayer.symbol}`);
       }
 
       // 縦が揃う
       if (
-        this.cells[0][i] === currentPlayer.symbol &&
-        this.cells[1][i] === currentPlayer.symbol &&
-        this.cells[2][i] === currentPlayer.symbol
+        this.cells[0][i] === this.currentPlayer.symbol &&
+        this.cells[1][i] === this.currentPlayer.symbol &&
+        this.cells[2][i] === this.currentPlayer.symbol
       ) {
-        console.log(`Winner: ${currentPlayer.symbol}`);
+        console.log(`Winner: ${this.currentPlayer.symbol}`);
       }
     }
 
     // 斜めが揃う
     if (
-      this.cells[0][0] === currentPlayer.symbol &&
-      this.cells[1][1] === currentPlayer.symbol &&
-      this.cells[2][2] === currentPlayer.symbol
+      this.cells[0][0] === this.currentPlayer.symbol &&
+      this.cells[1][1] === this.currentPlayer.symbol &&
+      this.cells[2][2] === this.currentPlayer.symbol
     ) {
-      console.log(`Winner: ${currentPlayer.symbol}`);
+      console.log(`Winner: ${this.currentPlayer.symbol}`);
     }
 
     // 斜めが揃う
     if (
-      this.cells[0][2] === currentPlayer.symbol &&
-      this.cells[1][1] === currentPlayer.symbol &&
-      this.cells[2][0] === currentPlayer.symbol
+      this.cells[0][2] === this.currentPlayer.symbol &&
+      this.cells[1][1] === this.currentPlayer.symbol &&
+      this.cells[2][0] === this.currentPlayer.symbol
     ) {
-      console.log(`Winner: ${currentPlayer.symbol}`);
+      console.log(`Winner: ${this.currentPlayer.symbol}`);
     }
   }
 }
@@ -82,6 +101,6 @@ class HumanPlayer extends Player {}
 class ComputerPlayer extends Player {}
 
 const board = new Board();
-const playerX = new HumanPlayer('x');
-const playerO = new HumanPlayer('o');
-let currentPlayer = playerX;
+// const playerX = new HumanPlayer('x');
+// const playerO = new HumanPlayer('o');
+// let currentPlayer = playerX;
