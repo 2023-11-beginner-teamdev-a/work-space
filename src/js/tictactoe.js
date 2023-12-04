21d802f6b9e7bd2fcc94a29adc185968ded43fe1
setodeve <keinick11@outlook.com>
2023-12-04 23:37:52 +0900

    refactor: 変数名、関数名、HTMLタグの変更

--- src/js/tictactoe.js
+++ src/js/tictactoe.js
@@ -35,19 +35,6 @@ export default class TicTacToe {
     this.clearCellClickListeners();
   }
 
-  get() {
-    let retString = localStorage.getItem('playResults');
-    // ローカルストレージ初回記録時に初期化
-    return retString === null ? Array(0) : JSON.parse(retString);
-  }
-
-  save(result) {
-    let retArray = this.get();
-    retArray.push(result);
-    localStorage.setItem('playResults', JSON.stringify(retArray));
-    this.displayResults();
-  }
-
   // セルをクリックした時のイベントリスナーの登録
   setCellClickListeners() {
     for (let row = 0; row < 3; row++) {
@@ -93,13 +80,13 @@ export default class TicTacToe {
       // 決着がついた場合
       this.winner = this.currentPlayer;
       console.log(`Winner is '${this.winner.symbol}'`);
-      this.save(this.winner.symbol);
+      this.savePlayResults(this.winner.symbol);
     } else {
       // 決着がついていない場合
       if (this.board.isBoardFull()) {
         // 引き分けの場合
         console.log('Draw!');
-        this.save('Draw');
+        this.savePlayResults('Draw');
       } else {
         // 試合続行の場合
         this.switchPlayer();
@@ -112,14 +99,29 @@ export default class TicTacToe {
     this.currentPlayer = this.currentPlayer === this.players.x ? this.players.o : this.players.x;
   }
 
+  // 勝敗の取得
+  getPlayResults() {
+    const localStorageStringData = localStorage.getItem('playResults');
+    // ローカルストレージ初回記録時に初期化
+    return localStorageStringData === null ? Array(0) : JSON.parse(localStorageStringData);
+  }
+
+  // 勝敗の保存
+  savePlayResults(result) {
+    let localStorageArrayData = this.getPlayResults();
+    localStorageArrayData.push(result);
+    localStorage.setItem('playResults', JSON.stringify(localStorageArrayData));
+    this.displayResults();
+  }
+
   // 勝敗の表示
   displayResults() {
     const resultsElement = document.getElementById(`results`);
     resultsElement.innerHTML = ``;
-    const results = this.get();
+    const results = this.getPlayResults();
     let message = '';
     results.forEach((result) => {
-      if (result == 'Draw') {
+      if (result === 'Draw') {
         message = result;
       } else {
         message = 'Win ' + result;
