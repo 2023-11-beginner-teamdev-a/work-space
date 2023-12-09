8f2ee19222e1b9f404f170383fab745fd5a5f7c0
setodeve <keinick11@outlook.com>
2023-12-09 17:11:37 +0900

    fix: 効果音ON設定、スタイリング修正

--- src/js/bgm.js
+++ src/js/bgm.js
@@ -1,16 +1,17 @@
 export default class BGM {
   constructor() {
     this.BGMElement = document.getElementById('toggle-BGM');
-    this.put = new Audio('../audio/試合終了のゴング.mp3');
+    this.put = new Audio('../src/audio/試合終了のゴング.mp3');
     this.toggleBGM = this.toggleBGM.bind(this);
   }
 
   toggleBGM() {
-    this.BGMElement.innerText = this.BGMElement.innerText === '効果音OFF' ? '効果音ON' : '効果音OFF';
-    if (this.BGMElement.innerText === '効果音ON'){
-      alert("勝敗決定時に効果音が出る設定になりました。音が出ても良い環境で実行してください")
+    this.BGMElement.innerText =
+      this.BGMElement.innerText === '効果音OFF' ? '効果音ON' : '効果音OFF';
+    if (this.BGMElement.innerText === '効果音OFF') {
+      alert('勝敗決定時に効果音が出る設定になりました。音が出ても良い環境で実行してください');
     } else {
-      alert("勝敗決定時に効果音が出ない設定になりました。")
+      alert('勝敗決定時に効果音が出ない設定になりました。');
     }
   }
 
@@ -18,10 +19,9 @@ export default class BGM {
     this.BGMElement.addEventListener('click', this.toggleBGM);
   }
 
-  playbgm(){
-    if (this.BGMElement.innerText==='効果音ON'){
+  playbgm() {
+    if (this.BGMElement.innerText === '効果音OFF') {
       this.put.play();
     }
   }
 }
-
